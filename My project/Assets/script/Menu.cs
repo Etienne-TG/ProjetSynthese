2e21a9014b2dd989c833039588c4f7134811bf84
dpironnet <sebastienpironnet5@gmail.com>
2024-11-11 16:06:27 -0500

    menu ( quitter, a propos ) + camera(pas encire fonctionnel)

--- My project/Assets/script/Menu.cs
+++ My project/Assets/script/Menu.cs
@@ -1,23 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    Canvas canvas;
+    Text messageTexte;
     bool choixJoueur1;
     bool choixJoueur2;
     bool choixJoueur3;
     bool choixJoueur4;
+    
+    public Vector3 targetPosition = new Vector3(0f, 0f, 0f);  // Position de la caméra
+    public Vector3 targetRotation = new Vector3(0f, 90f, 0f);    // Rotation de la caméra (en degrés)
+
     // Start is called before the first frame update
     void Start()
     {
+        canvas = GameObject.Find("Menu").GetComponent<Canvas>();
+        messageTexte = GetComponent<Text>();
         
     }
 
-    // Update is called once per frame
+    // Update is called once per frame  
     void Update()
     {
+        
         choixJoueur1 = Input.GetKeyDown(KeyCode.Keypad1);
         choixJoueur2 = Input.GetKeyDown(KeyCode.Keypad2);
         choixJoueur3 = Input.GetKeyDown(KeyCode.Keypad3);
@@ -26,6 +37,9 @@ public class Menu : MonoBehaviour
         if (choixJoueur1)
         {
             Debug.Log("1");
+            canvas.enabled = false;
+            Camera.main.transform.position = targetPosition;
+            Camera.main.transform.rotation = Quaternion.Euler(targetRotation);
         }
         else if (choixJoueur2)
         {
@@ -42,7 +56,10 @@ public class Menu : MonoBehaviour
         }
         else if (choixJoueur4)
         {
-            Debug.Log("4");
+            Debug.Log("4");  
+            messageTexte.enabled = true;
+            messageTexte.text = "Développeurs :\n Etienne Tardif-Girard\n Dylan Pironnet\n Commande : \n";
+           
         }
     }
 }
